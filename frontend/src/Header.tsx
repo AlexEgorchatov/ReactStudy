f01f7ce56a36f752bcb4db7da11c30b4ae2e38f6
Alex Egorchatov <alex.exorchatov@gmail.com>
2022-04-18 22:47:31 -0700

    Added an Icons component.

--- frontend/src/Header.tsx
+++ frontend/src/Header.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { UserIcon } from './Icons';
 
 export const Header = () => (
-  <div>
-    <a href="./">Q & A</a>
-    <input type="text" placeholder="Search..." />
-    <a href="./signin">
-      <span>Sign In</span>
-    </a>
-  </div>
+    <div>
+        <a href="./">Q & A</a>
+        <input type="text" placeholder="Search..." />
+        <a href="./signin">
+            <UserIcon />
+            <span>Sign In</span>
+        </a>
+    </div>
 );
