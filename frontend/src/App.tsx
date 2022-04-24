67fe88b936d5bebf83dedebe9a93332eabe3953d
Alex Egorchatov <alex.exorchatov@gmail.com>
2022-04-23 17:49:53 -0700

    Added routing.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,35 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import { css, jsx } from '@emotion/react';
-import React from 'react';
 import { Header } from './Header';
 import { HomePage } from './HomePage';
 import { fontFamily, fontSize, gray2 } from './Styles';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { AskPage } from './AskPage';
+import { SearchPage } from './SearchPage';
+import { SignInPage } from './SignInPage';
 
 function App() {
     return (
-        <div
-            className="App"
-            css={css`
-                font-family: ${fontFamily};
-                font-size: ${fontSize};
-                color: ${gray2};
-            `}
-        >
-            <Header />
-            <HomePage />
-        </div>
+        <BrowserRouter>
+            <div
+                className="App"
+                css={css`
+                    font-family: ${fontFamily};
+                    font-size: ${fontSize};
+                    color: ${gray2};
+                `}
+            >
+                <Header />
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="search" element={<SearchPage />} />
+                    <Route path="ask" element={<AskPage />} />
+                    <Route path="signin" element={<SignInPage />} />
+                    <Route path="home" element={<Navigate to="/" />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
     );
 }
 
