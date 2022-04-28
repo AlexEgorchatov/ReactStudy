b987acfaf893134ad8fd3dbaa24c9df2d0a1a963
Alex Egorchatov <alex.exorchatov@gmail.com>
2022-04-27 20:29:19 -0700

    Implemented lazy loading for the Ask page.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,11 +5,12 @@ import { Header } from './Header';
 import { HomePage } from './HomePage';
 import { fontFamily, fontSize, gray2 } from './Styles';
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
-import { AskPage } from './AskPage';
 import { SearchPage } from './SearchPage';
 import { SignInPage } from './SignInPage';
 import { QuestionPage } from './QuestionPage';
 import { NotFoundPage } from './NotFoundPage';
+import React, { lazy, Suspense } from 'react';
+const AskPage = lazy(() => import('./AskPage'));
 
 function App() {
     return (
@@ -26,7 +27,25 @@ function App() {
                 <Routes>
                     <Route path="/" element={<HomePage />} />
                     <Route path="search" element={<SearchPage />} />
-                    <Route path="ask" element={<AskPage />} />
+                    <Route
+                        path="ask"
+                        element={
+                            <Suspense
+                                fallback={
+                                    <div
+                                        css={css`
+                                            margin-top: 100px;
+                                            text-align: center;
+                                        `}
+                                    >
+                                        Loading...
+                                    </div>
+                                }
+                            >
+                                <AskPage />
+                            </Suspense>
+                        }
+                    ></Route>
                     <Route path="signin" element={<SignInPage />} />
                     <Route path="home" element={<Navigate to="/" />} />
                     <Route path="questions/:questionId" element={<QuestionPage />} />
