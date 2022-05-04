0a834217178dd2dd73e3646d3f5a772a0c5fbf4e
Alex Egorchatov <alex.exorchatov@gmail.com>
2022-05-03 17:23:05 -0700

    Added context provider.

--- frontend/src/Form.tsx
+++ frontend/src/Form.tsx
@@ -10,7 +10,7 @@ export interface Values {
 
 interface FormContextProps {
     values: Values;
-    setValue?: (fieldName: string, value: any) => void;
+    setContextValues?: (fieldName: string, value: any) => void;
 }
 
 export const FormContext = createContext<FormContextProps>({ values: {} });
@@ -22,29 +22,38 @@ interface Props {
 export const Form: FC<Props> = ({ submitCaption, children }) => {
     const [values, setValues] = useState<Values>({});
     return (
-        <form noValidate={true}>
-            <fieldset
-                css={css`
-                    margin: 10px auto 0 auto;
-                    padding: 30px;
-                    width: 350px;
-                    background-color: ${gray6};
-                    border-radius: 4px;
-                    border: 1px solid ${gray5};
-                    box-shadow: 0 3px 5px 0 rgba(0, 0, 0, 0.16);
-                `}
-            >
-                {children}
-                <div
+        <FormContext.Provider
+            value={{
+                values,
+                setContextValues: (fieldName: string, value: any) => {
+                    setValues({ ...values, [fieldName]: value });
+                },
+            }}
+        >
+            <form noValidate={true}>
+                <fieldset
                     css={css`
-                        margin: 30px 0px 0px 0px;
-                        padding: 20px 0px 0px 0px;
-                        border-top: 1px solid ${gray5};
+                        margin: 10px auto 0 auto;
+                        padding: 30px;
+                        width: 350px;
+                        background-color: ${gray6};
+                        border-radius: 4px;
+                        border: 1px solid ${gray5};
+                        box-shadow: 0 3px 5px 0 rgba(0, 0, 0, 0.16);
                     `}
                 >
-                    <PrimaryButton type="submit">{submitCaption}</PrimaryButton>
-                </div>
-            </fieldset>
-        </form>
+                    {children}
+                    <div
+                        css={css`
+                            margin: 30px 0px 0px 0px;
+                            padding: 20px 0px 0px 0px;
+                            border-top: 1px solid ${gray5};
+                        `}
+                    >
+                        <PrimaryButton type="submit">{submitCaption}</PrimaryButton>
+                    </div>
+                </fieldset>
+            </form>
+        </FormContext.Provider>
     );
 };
